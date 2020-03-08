6e17ccd4c2c21bae044c5acf4ee35f6316ae0900
Dinah <dinah@ewanet.ch>
2020-03-08 11:13:10 +0100

    Implemented Portfolio class which all tests work with.

--- src/main/java/portfolio/Portfolio.java
+++ src/main/java/portfolio/Portfolio.java
@@ -1,5 +1,52 @@
 package portfolio;
 
-public class Portfolio {
-    //TODO Complete this implementation and make it generic
+import portfolio.investments.Investment;
+import java.util.ArrayList;
+
+public class Portfolio<T extends Investment> {
+    ArrayList<T> investments = new ArrayList<>();
+
+    // contains
+    public boolean contains(T content) {
+        return investments.contains(content);
+    //    return investments.containsKey(content);
+    }
+
+    // buy
+    public void buy(T content) {
+        for (T i : investments) { // go through ArrayList
+            if (i.equals(content)) { // compare titles
+                i.setCount(i.getCount() + content.getCount()); // if an equal title is found, count should go up
+                return;
+            }
+        }
+        investments.add(content);
+    }
+
+    // sell
+    public void sell(String titel, int amount) {
+        T temp = null;
+        for (T i : investments) { // go through ArrayList
+
+            if (i.getTitel() == titel) { // compare titles
+                i.setCount(i.getCount() - amount); // if an equal title is found, count should go down
+
+                if (i.getCount() == 0) {
+                    temp = i;
+                }
+            }
+        }
+        investments.remove(temp);
+    }
+
+    // getShare
+    public T getShare(String titel){
+        for (T i : investments) { // go through ArrayList
+            if (i.getTitel() == titel) { // compare titles
+                return i;
+            }
+        }
+        return null;
+    }
+
 }
